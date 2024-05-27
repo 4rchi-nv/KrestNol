16b6c836e04e29a77332948f23fbe64d05445d64
4rchi-nv <arslan0agajanov@gmail.com>
2024-05-27 09:11:14 +0500

    game

---
+++ src/Board.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+function Square({ value, onSquareClick }) {
+
+    return(
+        <div onClick={onSquareClick} className="square">{ value }</div>
+    )
+}
+
+export function Board() {
+    const [xIsNext, setXIsNext] = useState(true)
+    const [squares, setSquares] = useState(Array(9).fill(null))
+    function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) return; 
+        const nextSqaure = squares.slice();
+        nextSqaure[i] = xIsNext ? "X" : "O"
+        setSquares(nextSqaure);
+        setXIsNext(!xIsNext);
+    };
+    const winner = calculateWinner(squares);
+    let status;
+    status = winner ? `Победитель: ${winner}` : `Кто ходит: ${xIsNext ? 'X' : 'O'}`;
+    return (
+        <>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => {handleClick(0)}} />
+                <Square value={squares[1]} onSquareClick={() => {handleClick(1)}} />
+                <Square value={squares[2]} onSquareClick={() => {handleClick(2)}} />
+                <Square value={squares[3]} onSquareClick={() => {handleClick(3)}} />
+                <Square value={squares[4]} onSquareClick={() => {handleClick(4)}} />
+                <Square value={squares[5]} onSquareClick={() => {handleClick(5)}} />
+                <Square value={squares[6]} onSquareClick={() => {handleClick(6)}} />
+                <Square value={squares[7]} onSquareClick={() => {handleClick(7)}} />
+                <Square value={squares[8]} onSquareClick={() => {handleClick(8)}} />
+            </div>
+        </>
+    )}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
